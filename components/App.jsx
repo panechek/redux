bfd2866f173c5467f59093ede9e316d72c159c6a
Anna Merkulova <panechek@inbox.ru>
2022-06-30 17:23:37 +0300

    first

---
+++ components/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from '../slices/counterSlice';
+
+export default () => {
+    const count = useSelector((state) => state.counter.value);
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <div>
+                <button
+                aria-label="Increment value"
+                onClick={() => dispatch(increment())}
+                >
+                Прибавить
+                </button>
+                <span>{count}</span>
+                <button
+                aria-label="Decrement value"
+                onClick={() => dispatch(decrement())}
+                >
+                    Отнять
+                </button>
+            </div>
+        </div>
+    );
+};
